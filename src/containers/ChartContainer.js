e8109692c6737bc7ad62e2fb00473279c0f828bb
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-04-25 06:41:07 +0100

    Update App.js to import ChartContainer and stub out ChartContainer

---
+++ src/containers/ChartContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class ChartContainer extends React.Component {
+
+  render(){
+    return(
+      <article>
+        <h2>iTunes Top 20</h2>
+      </article>
+    );
+  }
+}
+
+export default ChartContainer;
